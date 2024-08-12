2d2a3cf9364909a69fee42a999412394fc2d9cd2
Sören Fetsch <121955157+soerenfetsch@users.noreply.github.com>
2024-08-12 15:40:41 +0200

    Small update to features; Created training for coin collector task

--- agent_code/aenormendeas_agent/callbacks.py
+++ agent_code/aenormendeas_agent/callbacks.py
@@ -9,6 +9,9 @@ ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
 
 N_CLOSEST_COINS = 1 # number of closest coins to consider for features
 
+Q_TABLE_FILE = "coin_collector_qtable.pkl"
+TRAIN_NEW = False
+
 
 def setup(self):
     """
@@ -24,49 +27,58 @@ def setup(self):
 
     :param self: This object is passed to all callbacks and you can set arbitrary values.
     """
-    if not self.train:
-        qtable_file = "SOME FILE NAME"
+    if not TRAIN_NEW:
+        qtable_file = Q_TABLE_FILE
     else:
         qtable_file = None
-    if self.train or not os.path.isfile(qtable_file):
+    if TRAIN_NEW and self.train or not os.path.isfile(qtable_file):
         self.logger.info("Setting up Q-Table from scratch.")
         self.q_table = {}
     else:
         self.logger.info("Loading Q-Table from saved state.")
         with open(qtable_file, "rb") as file:
             self.q_table = pickle.load(file)
+        self.logger.debug(f"Loaded saved Q-Table with {len(self.q_table)} entries.")
 
 
 def act(self, game_state: dict) -> str:
     """
     Your agent should parse the input, think, and take a decision.
     When not in training mode, the maximum execution time for this method is 0.5s.
+    We use an epsilon parameter during training for exploration. Otherwise convert
+    the game state to a feature vector and select the best actions from the q_table
+    given the feature_vector as key.
 
     :param self: The same object that is passed to all of your callbacks.
     :param game_state: The dictionary that describes everything on the board.
     :return: The action to take as a string.
     """
-    # todo Exploration vs exploitation
-    random_prob = .1
-    if self.train and random.random() < random_prob:
-        self.logger.debug("Choosing action purely at random.")
+    # Exploration vs exploitation
+    if game_state is None or (self.train and np.random.rand() < self.epsilon):
+        self.logger.debug("Epsilon Exploration: Choosing action purely at random.")
         # 80%: walk in any direction. 10% wait. 10% bomb.
         return np.random.choice(ACTIONS, p=[.2, .2, .2, .2, .1, .1])
+    self.logger.debug("Exploitation: Selecting best action using Q-Value.")
+    features = state_to_features(self, game_state)
+    q_values: dict = self.q_table.get(tuple(features),
+                                      {a: 0.0 for a in ACTIONS})
+    # print(f"Given features: {features}, Q-values: {q_values}")
+    max_q_value = max(q_values.values())
+    best_actions = [a for a, q in q_values.items() if q == max_q_value]
+    return np.random.choice(best_actions)
 
-    self.logger.debug("Querying model for action.")
-    return np.random.choice(ACTIONS, p=self.model)
-
-def adjacent_tile_features(game_state, agent_position):
+def adjacent_tile_features(self, game_state, agent_position):
     """ 
     Convert the agent's position and the game state to features for the 4 adjacent
     tiles UP, DOWN, LEFT, RIGHT. We encode each tile in binary:
-    [0, 0]: outside bound, [0, 1]: stone wall, [1, 0]: crate,
-    [1, 1]: free tile
+    0: outside bound, 1: stone wall, 2: crate,
+    3: free tile
     
     :param: game_state (dict): The dictionary that describes everything on the board.
     :agent_position (np.array([x, y])): current coordinates of the agent
-    :return: np.array(list)[4 x 2]: concatenated tile features UP, DOWN, LEFT, RIGHT
+    :return: np.array(list)[4]: concatenated tile features UP, DOWN, LEFT, RIGHT
     """
+    self.logger.debug("Converting adjacent fields to features")
     y, x = agent_position
     height, width = game_state['field'].shape
     tile_features = []
@@ -75,33 +87,34 @@ def adjacent_tile_features(game_state, agent_position):
     for dy, dx in directions:
         # Outside bound
         if not (0 <= dx < width and 0 <= dy < height):
-            tile_features.extend([0, 0])
+            tile_features.append(0)
         # Stone wall
         elif game_state['field'][dy, dx] == -1:
-            tile_features.extend([0, 1])
+            tile_features.append(1)
         # Crate
         elif game_state['field'][dy, dx] == 1:
-            tile_features.extend([1, 0])
+            tile_features.append(2)
         # Free tile
         elif game_state['field'][dy, dx] == 0:
-            tile_features.extend([1, 1])
+            tile_features.append(3)
 
-    assert len(tile_features) == 8
+    assert len(tile_features) == 4
     return np.array(tile_features)
 
 # TODO: add objectives crates and bombs
-def find_closest_objectives(game_state, agent_position):
+def find_closest_objectives(self, game_state, agent_position):
     """
     Use Dijkstra's algorithm to construct shortest path search
     and find the closest coins/crates/bombs up the the max number
     specified in the setup. We construct a parent tree to store
     the movement UP, DOWN, LEFT, RIGHT adjacent to the agent as
     well as the respective distance as a feature for each objective.
-    UP: [0, 0], DOWN: [0, 1], LEFT: [1, 0], RIGHT: [1, 1].
+    WAIT: -1, UP: 0, DOWN: 1, LEFT: 2, RIGHT: 3.
     :params: game_state (dict): The dictionary that describes everything on the board.
     :agent_position (np.array([x, y])): current coordinates of the agent
     :return: np.array(list): concatenated tile features for all objectives
     """
+    self.logger.debug("Finding closest coin objectives.")
     y, x = agent_position
     # UP DOWN LEFT RIGHT
     neighbors = [(y-1, x), (y+1, x), (y, x-1), (y, x+1)]
@@ -120,12 +133,16 @@ def find_closest_objectives(game_state, agent_position):
         if curr_dist > distances[dx, dy]:
             continue
         if (dy, dx) in game_state['coins']:
-            ddy, ddx = parents[dy, dx]
-            while (ddy, ddx) not in neighbors:
-                ddy, ddx = parents[ddy, ddx]
-            index = neighbors.index((ddy, ddx))
-            out_features[coins_picked] = [index // 2, index % 2, curr_dist]
-            coins_picked += 1
+            if (dy, dx) == (y, x):
+                out_features[coins_picked] = [-1, curr_dist]
+                coins_picked += 1
+            else:
+                ddy, ddx = dy, dx
+                while (ddy, ddx) not in neighbors:
+                    ddy, ddx = parents[ddy, ddx]
+                index = neighbors.index((ddy, ddx))
+                out_features[coins_picked] = [index, curr_dist]
+                coins_picked += 1
         directions = [(dy-1, dx), (dy+1, dx), (dy, dx-1), (dy, dx+1)]
         
         for ddy, ddx in directions:
@@ -137,14 +154,11 @@ def find_closest_objectives(game_state, agent_position):
                 distances[ddy, ddx] = curr_dist + 1
                 parents[ddy, ddx] = (dy, dx)
                 heapq.heappush(q, (curr_dist+1, (ddy, ddx)))
-
     return np.concatenate(out_features)
 
 
-def state_to_features(game_state: dict) -> np.array:
+def state_to_features(self, game_state: dict) -> np.array:
     """
-    *This is not a required function, but an idea to structure your code.*
-
     Converts the game state to the input of your model, i.e.
     a feature vector.
 
@@ -160,11 +174,16 @@ def state_to_features(game_state: dict) -> np.array:
         return None
 
     agent_position = game_state['self'][3]
-    return np.concatenate([adjacent_tile_features(game_state, agent_position),
-                           find_closest_objectives(game_state, agent_position)])
+    return np.concatenate([adjacent_tile_features(self, game_state, agent_position),
+                           find_closest_objectives(self, game_state, agent_position)])
 
 
 if __name__ == "__main__":
+    class object:
+        class logg:
+            def debug(self, str):
+                print(str)
+        logger = logg()
     # Test the feature vector
     game_state = {
         'round': 0,
@@ -176,6 +195,6 @@ if __name__ == "__main__":
         'coins': [(0, 0), (1, 2), (1, 3), (3, 3)],
         'self': ("Name", 0, True, (3, 0))
     }
-    features = state_to_features(game_state)
+    features = state_to_features(object(), game_state)
     print(features)
-    assert (features == np.array([0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 3])).all()
+    assert (features == np.array([1, 0, 0, 3, 3, 3])).all()
--- agent_code/aenormendeas_agent/train.py
+++ agent_code/aenormendeas_agent/train.py
@@ -4,18 +4,21 @@ import pickle
 from typing import List
 
 import events as e
-from .callbacks import state_to_features
+from .callbacks import state_to_features, N_CLOSEST_COINS, Q_TABLE_FILE, ACTIONS
 
 # This is only an example!
 Transition = namedtuple('Transition',
                         ('state', 'action', 'next_state', 'reward'))
 
 # Hyper parameters -- DO modify
-TRANSITION_HISTORY_SIZE = 3  # keep only ... last transitions
+TRANSITION_HISTORY_SIZE = 1000  # keep only ... last transitions
 RECORD_ENEMY_TRANSITIONS = 1.0  # record enemy transitions with probability ...
 
 # Events
 PLACEHOLDER_EVENT = "PLACEHOLDER"
+# Custom events
+MOVE_CLOSER_TO_COIN = "MOVE_CLOSER_TO_COIN"
+MOVE_AWAY_FROM_COIN = "MOVE_AWAY_FROM_COIN"
 
 
 def setup_training(self):
@@ -26,10 +29,60 @@ def setup_training(self):
 
     :param self: This object is passed to all callbacks and you can set arbitrary values.
     """
-    # Example: Setup an array that will note transition tuples
-    # (s, a, r, s')
+    # replay buffer to store experiences
     self.transitions = deque(maxlen=TRANSITION_HISTORY_SIZE)
+    # set up training parameters for exploration and learning
+    # TODO: Maybe do epsilon decay starting with lots of exploration?
+    self.epsilon = 0.2
+    self.gamma = 0.9
+    self.alpha = 0.1
 
+    # TODO: Add more rewards for upcoming tasks
+    self.game_rewards = {
+        e.COIN_COLLECTED: 150.0,
+        e.KILLED_SELF: -50.0,
+        e.GOT_KILLED: -200.0,
+        PLACEHOLDER_EVENT: 0.0,
+        e.INVALID_ACTION: -200.0,
+        MOVE_CLOSER_TO_COIN: 30.0,
+        MOVE_AWAY_FROM_COIN: -10.0,
+        e.WAITED: 0.0,
+    }
+
+def update_q_values(self):
+    """ 
+    Update Q-Values based on the Bellman equation by sampling batches
+    from the transitions buffer.
+    
+    :param self: This object is passed to all callbacks and you can set arbitrary values.
+    """
+    while self.transitions:
+        transition = self.transitions.popleft()
+        state = transition.state
+        action = transition.action
+        reward = transition.reward
+        next_state = transition.next_state
+
+        # Get Q-Value from table else get 0.0
+        q_value = self.q_table.get(tuple(state), {}).get(action, 0.0)
+        # Update the Q-Value based on Bellman equation update rule
+        if next_state is None:
+            new_reward = reward
+        else:
+            next_q_values: dict = self.q_table.get(tuple(next_state),
+                                             {a: 0.0 for a in ACTIONS})
+            max_next_q_value = max(next_q_values.values())
+            new_reward = reward + self.gamma * max_next_q_value
+        new_q_value = q_value + self.alpha * (new_reward - q_value)
+
+        # Update the Q-table
+        if tuple(state) not in self.q_table:
+            self.q_table[tuple(state)] = {}
+        self.q_table[tuple(state)][action] = new_q_value
+
+    # Save the Q-Table as pkl file
+    with open(Q_TABLE_FILE, "wb") as file:
+        pickle.dump(self.q_table, file)
 
 def game_events_occurred(self, old_game_state: dict, self_action: str, new_game_state: dict, events: List[str]):
     """
@@ -49,13 +102,19 @@ def game_events_occurred(self, old_game_state: dict, self_action: str, new_game_
     :param events: The events that occurred when going from  `old_game_state` to `new_game_state`
     """
     self.logger.debug(f'Encountered game event(s) {", ".join(map(repr, events))} in step {new_game_state["step"]}')
+    old_features = state_to_features(self, old_game_state)
+    new_features = state_to_features(self, new_game_state)
 
-    # Idea: Add your own events to hand out rewards
-    if ...:
-        events.append(PLACEHOLDER_EVENT)
+    # Custom events to hand out reward
+    # event for moving closer to closest coin / farther away
+    if e.COIN_COLLECTED not in events:
+        if new_features[5] < old_features[5]:
+            events.append(MOVE_CLOSER_TO_COIN)
+        elif new_features[5] > old_features[5]:
+            events.append(MOVE_AWAY_FROM_COIN)
 
     # state_to_features is defined in callbacks.py
-    self.transitions.append(Transition(state_to_features(old_game_state), self_action, state_to_features(new_game_state), reward_from_events(self, events)))
+    self.transitions.append(Transition(old_features, self_action, new_features, reward_from_events(self, events)))
 
 
 def end_of_round(self, last_game_state: dict, last_action: str, events: List[str]):
@@ -72,28 +131,26 @@ def end_of_round(self, last_game_state: dict, last_action: str, events: List[str
     :param self: The same object that is passed to all of your callbacks.
     """
     self.logger.debug(f'Encountered event(s) {", ".join(map(repr, events))} in final step')
-    self.transitions.append(Transition(state_to_features(last_game_state), last_action, None, reward_from_events(self, events)))
-
-    # Store the model
-    with open("my-saved-model.pt", "wb") as file:
-        pickle.dump(self.model, file)
-
+    # TODO: Maybe end of game events?
+    # Update the Q Values
+    self.transitions.append(Transition(
+        state_to_features(self, last_game_state), last_action, None, reward_from_events(self, events)
+    ))
+    update_q_values(self)
 
 def reward_from_events(self, events: List[str]) -> int:
     """
-    *This is not a required function, but an idea to structure your code.*
+    Reward or punish the agent depending on the occurred events. The game_rewards
+    is created in the setup_training method and can be adjusted there. Returns
+    the sum of all rewards.
 
-    Here you can modify the rewards your agent get so as to en/discourage
-    certain behavior.
+    :param self: The same object that is passed to all of your callbacks.
+    :param events (List[str]): list of events occurred in the last step
+    :return sum (float): sum of all rewards for all events occurred
     """
-    game_rewards = {
-        e.COIN_COLLECTED: 1,
-        e.KILLED_OPPONENT: 5,
-        PLACEHOLDER_EVENT: -.1  # idea: the custom event is bad
-    }
     reward_sum = 0
     for event in events:
-        if event in game_rewards:
-            reward_sum += game_rewards[event]
+        if event in self.game_rewards:
+            reward_sum += self.game_rewards[event]
     self.logger.info(f"Awarded {reward_sum} for events {', '.join(events)}")
     return reward_sum
